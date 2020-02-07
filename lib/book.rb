0eadedf42a6e2ff3774523d20a484abc80d8ea44
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-02-07 03:49:53 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,16 @@
 class Book
   # attr_accessor(:title, :author)
-  def initialize(title,author)
+  def initialize(title, author)
     @title= title
     @author= author
   end
 
   def title
     @title
+  end
+
+  def author=(author)
+    @author= author
   end
 
   def author
