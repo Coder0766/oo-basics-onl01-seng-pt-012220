481a518d2a8d18110f5e20d166e286c1bcc23383
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-02-07 03:44:41 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+  # attr_accessor(:title, :author_name)
+  def initialize(title,author_name)
+    @title= title
+    @author= author_name
+  end
 
+  def title
+    @title
+  end
 
+  def author_name
+    @author_name
+  end
+
+end
